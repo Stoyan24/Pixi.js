bff03076b158aab75aa4882f1ee1974dec0252a8
Stoyan24 <stoiansmilqnov24@gmail.com>
2018-06-07 16:03:13 +0300

    Solution to Dopamin task(i hope this is the right solution)

--- things.js
+++ things.js
@@ -1,9 +1,21 @@
 global.atob = require("atob");
+//npm install atob --save
 
-function f() {
+function Decoder() {
     let string = 'ICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIA0KLi0uICAgICAuICAgICAgIC4gICAgICAgICAgIC4gICAgICAgICAuICAgIA0KfC0uLS4gIC18Li0uICAuLXwuLS4tLi0uLi0uLS4tLi0sICAgICB8LS4tLg0KJy1gLScgICAnYC0nICBgLSdgLXwtYC1gJyAnICcgYCctICBvICBgLWAtfA0KICAgICAgICAgICAgICAgICAgJyAgICAgICAgICAgICAgICAgICAgYC0nDQogICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgDQogIC4gIC4gICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgDQouLXwuLXwtLiAuLS4gICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgDQpgLSdgJ2AtJy1gLXwgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgDQogICAgICAgICBgLScgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAg';
     let decodedString = atob(string);
     console.log(decodedString);
-}
 
-f()
+
+}
+Decoder();
+
+//.-.     .       .           .         .
+// |-.-.  -|.-.  .-|.-.-.-..-.-.-.-,     |-.-.
+// '-`-'   '`-'  `-'`-|-`-`' ' ' `'-  o  `-`-|
+//                   '                    `-'
+//
+//   .  .
+// .-|.-|-. .-.
+// `-'`'`-'-`-|
+//          `-'
